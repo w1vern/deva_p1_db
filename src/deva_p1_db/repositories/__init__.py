958b7454512a8f44b32c06101d531898ced67b1e
w1vern <bndvvl@gmail.com>
2025-04-17 00:58:21 +0300

    upd

--- src/deva_p1_db/repositories/__init__.py
+++ src/deva_p1_db/repositories/__init__.py
@@ -1,6 +1,6 @@
 
 from .file_repository import FileRepository
-from .file_type_repository import FileTypeRepository
 from .invited_users_repository import InvitedUsersRepository
 from .project_repository import ProjectRepository
 from .user_repository import UserRepository
+from .task_repository import TaskRepository
